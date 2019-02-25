1a58c1c2d80c24f2a716fa5fa2942cb685d1e4c0
Kris Sunu Purnandaru <kris.sunupurnandaru@ai.astra.co.id>
2019-02-25 13:44:00 +0700

    Create and modify directives

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import Quote from './Quote.vue'
 import Forms from './Forms.vue'
+import Directive from "./Directive";
 
 export const eventBus = new Vue({
   //centralized eventBus so you can use it everywhere
@@ -12,17 +13,38 @@ export const eventBus = new Vue({
   }
 });
 
-/*new Vue({
+/*Global directive*/
+Vue.directive('highlight', {
+  bind(el, binding, vnode) {
+    //el.style.backgroundColor = 'cyan';
+    //el.style.backgroundColor = binding.value;
+    var delay = 0;
+    if (binding.modifiers['delayed']) {
+      delay = 3000;
+    }
+
+    setTimeout(() => {
+      if (binding.arg === 'background') {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay);
+  }
+});
+
+new Vue({
   el: '#app',
-  render: h => h(App)
-});*/
+  //render: h => h(App)
+  render: h => h(Directive)
+});
 
 /*new Vue({
   el: '#quote',
   render: h => h(Quote)
 });*/
 
-new Vue({
+/*new Vue({
   el: '#forms',
   render: h => h(Forms)
-});
+});*/
